e0b91d0d2156424f0984b9dabace17f71d35f389
2025sl93008 <2025sl93008@wilp.bits-pilani.ac.in>
2026-04-05 21:45:35 +0530

    add division functionality

--- calculator.py
+++ calculator.py
@@ -2,5 +2,12 @@ def add(a,b):
 	return a + b
 def multiply(a,b):
 	return a * b
+<<<<<<< HEAD
 def subtract(a,b):
 	return a - b
+=======
+def divide(a,b):
+	if b==0:
+		return None
+	return a/b
+>>>>>>> 8e6b2b9 (add division functionality)
